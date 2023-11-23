4f419509c760e173d25300212f608f8ebc4ae7e4
Y7T007 <yassir7t@gmail.com>
2023-11-23 10:48:39 +0100

    Q2 - FINAL

--- TP2/Q2.sql
+++ TP2/Q2.sql
@@ -1,28 +1,44 @@
 SET SERVEROUTPUT ON;
 
 DECLARE
-    CURSOR C_PILOTES_UPDATE IS 
-        SELECT * FROM PILOTE 
-        WHERE SALAIRE < (SELECT AVG(SALAIRE) FROM PILOTE) FOR UPDATE;
+    CURSOR C_PILOTES_UPDATE IS
+        SELECT
+            *
+        FROM
+            PILOTE
+        WHERE
+            SALAIRE < (
+                SELECT
+                    AVG(SALAIRE)
+                FROM
+                    PILOTE
+            ) FOR UPDATE;
 
-    TYPE TYPE_TABLE_PILOTES IS TABLE OF PILOTE%ROWTYPE;
+    TYPE TYPE_TABLE_PILOTES IS
+        TABLE OF PILOTE%ROWTYPE;
 
     TABLE_PILOTES TYPE_TABLE_PILOTES;
 
 BEGIN
 
+    FOR PILOTE IN C_PILOTES_UPDATE LOOP
 
-    FOR PILOTE IN C_PILOTES_UPDATE
-    LOOP
-        UPDATE PILOTE 
-        SET SALAIRE = SALAIRE + SALAIRE * 0.2
-        WHERE CURRENT OF C_PILOTES_UPDATE;
+    -- QUESTION 2 - A :
+        UPDATE PILOTE
+        SET
+            SALAIRE = SALAIRE + SALAIRE * 0.2
+        WHERE
+            CURRENT OF C_PILOTES_UPDATE;
 
+    -- QUESTION 2 - B :
         IF C_PILOTES_UPDATE%FOUND THEN
-                DBMS_OUTPUT.PUT_LINE('NOM PILOTE: ' || PILOTE.NOM || ', SALAIRE: ' || PILOTE.SALAIRE);
+            DBMS_OUTPUT.PUT_LINE('NOM PILOTE: '
+                                || PILOTE.NOM
+                                || ', SALAIRE: '
+                                || PILOTE.SALAIRE);
         END IF;
 
     END LOOP;
-    
+
 END;
 /
