424e32da7303a0ee7bdc48c85e31d1121987a5a8
Y7T007 <yassir7t@gmail.com>
2023-11-23 12:42:56 +0100

    CR

---
+++ TP2/COMPTE_RENDU_YASSIR_WAHID.sql
@@ -0,0 +1,254 @@
+
+/****************************************************
+*                PL SQL    - TP 2                   *
+*    Les structures de contrôles en PL/SQL          *
+*****************************************************
+*                                                   *
+* NOM: WAHID                                        *
+* PRENOM: YASSIR                                    *
+* NIVEAU: GI2                                       *
+* ENCADRE PAR: Prof. YASSER MASMOUDI                *
+*                                                   *
+****************************************************/
+
+
+
+-- QUESTION 1 -A
+
+DECLARE
+    TYPE RECORD_DE_PILOTE IS RECORD(
+        ID_PIL INTEGER,
+        NOM VARCHAR2(50),
+        AGE INTEGER,
+        VILLE VARCHAR2(25),
+        SALAIRE INTEGER,
+        HEURE_VOLS INTEGER
+    );
+    TYPE TABLE_DE_PILOTES IS
+        TABLE OF RECORD_DE_PILOTE INDEX BY PLS_INTEGER;
+
+    PILOTES TABLE_DE_PILOTES;
+
+    NB_PILOTES INTEGER;
+
+BEGIN
+
+    PILOTES(1).ID_PIL:=1;
+    PILOTES(1).NOM:='YASSIR';
+    PILOTES(1).AGE:=21;
+    PILOTES(1).VILLE:='CASABLANCA';
+    PILOTES(1).SALAIRE:=15000;
+    PILOTES(1).HEURE_VOLS:=150;
+
+    PILOTES(2).ID_PIL:=12;
+    PILOTES(2).NOM:='NOM 12';
+    PILOTES(2).AGE:=12;
+    PILOTES(2).VILLE:='VILLE 12';
+    PILOTES(2).SALAIRE:=12;
+    PILOTES(2).HEURE_VOLS:=12;
+
+    PILOTES(3).ID_PIL:=3;
+    PILOTES(3).NOM:='NOM 3';
+    PILOTES(3).AGE:=3;
+    PILOTES(3).VILLE:='VILLE 3';
+    PILOTES(3).SALAIRE:=3;
+    PILOTES(3).HEURE_VOLS:=3;
+
+    PILOTES(4).ID_PIL:=4;
+    PILOTES(4).NOM:='NOM 4';
+    PILOTES(4).AGE:=4;
+    PILOTES(4).VILLE:='VILLE 4';
+    PILOTES(4).SALAIRE:=4;
+    PILOTES(4).HEURE_VOLS:=4;
+
+    PILOTES(5).ID_PIL:=5;
+    PILOTES(5).NOM:='NOM 5';
+    PILOTES(5).AGE:=5;
+    PILOTES(5).VILLE:='VILLE 5';
+    PILOTES(5).SALAIRE:=5;
+    PILOTES(5).HEURE_VOLS:=5;
+
+    -- AFFICHAGE DES RESULTATS:
+    FOR I IN 1..5 LOOP
+        DBMS_OUTPUT.PUT_LINE('NOM : '||PILOTES(I).NOM);
+    END LOOP;
+
+    -- AFFICHAGE DES NOMBRES DE PILOTES :
+    NB_PILOTES:= PILOTES.COUNT;
+    DBMS_OUTPUT.PUT_LINE('LE NOMBRE TOTALE DES PILOTES EST : '||NB_PILOTES);
+
+END;
+/
+
+
+
+-- QUESTION 1 - B;
+
+DECLARE
+
+    RECORD_DE_PILOTE PILOTE%ROWTYPE;
+
+    TYPE TABLE_DE_PILOTES IS TABLE OF RECORD_DE_PILOTE%TYPE INDEX BY PLS_INTEGER;
+
+    PILOTES TABLE_DE_PILOTES;
+
+    V_OUTPUT VARCHAR2(150);
+
+BEGIN
+
+-- REMPLISSAGE DE LA TABLE :
+
+    PILOTES(1).ID_PIL:=1;
+    PILOTES(1).NOM:='YASSIR';
+    PILOTES(1).AGE:=21;
+    PILOTES(1).VILLE:='CASABLANCA';
+    PILOTES(1).SALAIRE:=15000;
+    PILOTES(1).HEURE_VOLS:=150;
+
+    PILOTES(2).ID_PIL:=12;
+    PILOTES(2).NOM:='NOM 12';
+    PILOTES(2).AGE:=12;
+    PILOTES(2).VILLE:='VILLE 12';
+    PILOTES(2).SALAIRE:=12;
+    PILOTES(2).HEURE_VOLS:=12;
+
+    PILOTES(3).ID_PIL:=3;
+    PILOTES(3).NOM:='NOM 3';
+    PILOTES(3).AGE:=3;
+    PILOTES(3).VILLE:='VILLE 3';
+    PILOTES(3).SALAIRE:=3;
+    PILOTES(3).HEURE_VOLS:=3;
+
+    PILOTES(4).ID_PIL:=4;
+    PILOTES(4).NOM:='NOM 4';
+    PILOTES(4).AGE:=4;
+    PILOTES(4).VILLE:='VILLE 4';
+    PILOTES(4).SALAIRE:=4;
+    PILOTES(4).HEURE_VOLS:=4;
+
+    PILOTES(5).ID_PIL:=5;
+    PILOTES(5).NOM:='NOM 5';
+    PILOTES(5).AGE:=5;
+    PILOTES(5).VILLE:='VILLE 5';
+    PILOTES(5).SALAIRE:=5;
+    PILOTES(5).HEURE_VOLS:=5;
+
+    -- AFFICHAGE DES RESULTATS:
+    FOR I IN 1..5 LOOP
+        DBMS_OUTPUT.PUT_LINE('NOM : '||PILOTES(I).NOM);
+    END LOOP;
+
+END;
+/
+
+
+-- QUESTION 2 : 
+
+SET SERVEROUTPUT ON;
+
+DECLARE
+    CURSOR C_PILOTES_UPDATE IS
+        SELECT
+            *
+        FROM
+            PILOTE
+        WHERE
+            SALAIRE < (
+                SELECT
+                    AVG(SALAIRE)
+                FROM
+                    PILOTE
+            ) FOR UPDATE;
+
+    TYPE TYPE_TABLE_PILOTES IS
+        TABLE OF PILOTE%ROWTYPE;
+
+    TABLE_PILOTES TYPE_TABLE_PILOTES;
+
+BEGIN
+
+    FOR PILOTE IN C_PILOTES_UPDATE LOOP
+
+    -- QUESTION 2 - A :
+        UPDATE PILOTE
+        SET
+            SALAIRE = SALAIRE + SALAIRE * 0.2
+        WHERE
+            CURRENT OF C_PILOTES_UPDATE;
+
+    -- QUESTION 2 - B :
+        IF C_PILOTES_UPDATE%FOUND THEN
+            DBMS_OUTPUT.PUT_LINE('NOM PILOTE: '
+                                || PILOTE.NOM
+                                || ', SALAIRE: '
+                                || PILOTE.SALAIRE);
+        END IF;
+
+    END LOOP;
+    
+END;
+/
+
+
+-- QUESTION 3 :
+
+SET SERVEROUTPUT ON;
+
+DECLARE
+
+    V_VOL VARCHAR2(8);
+    V_NUM NUMBER(3);
+    V_DATE DATE;
+
+BEGIN
+
+    V_VOL := 'V10&___INTRODUISEZ_VOTRE_NUMERO_DE_VOL';
+    V_NUM := '&___INTRODUISEZ_VOTRE_NUMERO_DE_PASSAGER';
+    V_DATE := TO_DATE('&___INTRODUISEZ_LA_DATE_DE_DEPART_SOUS_CETTE_FORME__AAAA_MM_JJ','YYYY-MM-DD');
+    DBMS_OUTPUT.PUT_LINE('DATE : '||V_DATE);
+
+    IF V_DATE > SYSDATE 
+    THEN
+
+        INSERT INTO RESERVATION(N_VOL,N_PASSAGER,DATE_RES) 
+        VALUES ( V_VOL,V_NUM,V_DATE);
+
+        IF SQL%FOUND THEN COMMIT; END IF;
+
+    ELSE
+
+    DBMS_OUTPUT.PUT_LINE('DATE INVALID MERCI DE REESSAYEER....');
+
+    END IF;
+
+END;
+/
+
+
+
+-- QUESTION 4 : 
+
+SET SERVEROUTPUT ON;
+
+DECLARE
+    SOMME_SALAIRES1 NUMBER := 0 ;
+    SOMME_SALAIRES2 NUMBER := 0 ;
+BEGIN
+    SELECT SUM(SALAIRE)
+        INTO SOMME_SALAIRES1
+        FROM PILOTE WHERE AGE > 40;
+    DBMS_OUTPUT.PUT_LINE('LA SOMME DES SALAIRES PAR SELECTION EST : ' || SOMME_SALAIRES1);
+
+    FOR PILOTE IN (SELECT * FROM PILOTE) LOOP
+        IF PILOTE.AGE > 40 THEN
+            SOMME_SALAIRES2 := SOMME_SALAIRES2 + PILOTE.SALAIRE;
+        END IF;
+    END LOOP;
+    
+    DBMS_OUTPUT.PUT_LINE('LA SOMME DES SALAIRES PAR LOOP FOR EST : ' || SOMME_SALAIRES2);
+
+
+END;
+/
+
+
