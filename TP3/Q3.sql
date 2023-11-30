b7fa23a0ceda2d88f55fd66dfc7b2ccf444c604b
Y7T007 <yassir7t@gmail.com>
2023-11-30 13:02:00 +0100

    Q3

--- TP3/Q3.sql
+++ TP3/Q3.sql
@@ -3,7 +3,7 @@ DECLARE
     IS
         SELECT *
             FROM YASSIR.VOL
-            ORDER BY (DATE_ARRIVEE-DATE_DEPART)
+            ORDER BY (DATE_ARRIVEE-DATE_DEPART) 
             FETCH FIRST 1 ROW ONLY ;
 
     VOL_PLUS_COURT YASSIR.VOL%ROWTYPE;
@@ -13,6 +13,7 @@ DECLARE
     JOURS INTEGER;
 
     DUREE_TOTAL FLOAT;
+    DUREE_CONVERTIE INTERVAL DAY TO SECOND;
 
 
 BEGIN
@@ -20,6 +21,7 @@ BEGIN
     OPEN c_VOLS_PLUS_COURT;
 
     FETCH c_VOLS_PLUS_COURT INTO VOL_PLUS_COURT;
+
 
         DBMS_OUTPUT.PUT_LINE('NUM DU VOL : ' || VOL_PLUS_COURT.ID_VOL);
         DBMS_OUTPUT.PUT_LINE('----------------------------------------------'); 
@@ -35,6 +37,35 @@ BEGIN
         DBMS_OUTPUT.PUT_LINE('NUM AVION : ' || VOL_PLUS_COURT.N_AVION);
         DBMS_OUTPUT.PUT_LINE('NUM PILOTE : ' || VOL_PLUS_COURT.N_PILOTE);
         DBMS_OUTPUT.PUT_LINE('----------------------------------------------'); 
+
+        DUREE_TOTAL := (VOL_PLUS_COURT.DATE_ARRIVEE-VOL_PLUS_COURT.DATE_DEPART);
+        JOURS := TO_NUMBER(DUREE_TOTAL);
+        HEURES := TO_NUMBER(DUREE_TOTAL*24);
+        MINUTES := TO_NUMBER(DUREE_TOTAL*24*60);
+
+        DBMS_OUTPUT.PUT_LINE('J  : ' || JOURS);
+        DBMS_OUTPUT.PUT_LINE('H  : ' || HEURES);
+        DBMS_OUTPUT.PUT_LINE('M  : ' || MINUTES);
+
+        DUREE_CONVERTIE := NUMTODSINTERVAL((MINUTES*60), 'SECOND');
+
+        JOURS := EXTRACT(DAY FROM DUREE_CONVERTIE);
+        HEURES := EXTRACT(HOUR FROM DUREE_CONVERTIE);
+        MINUTES := EXTRACT(MINUTE FROM DUREE_CONVERTIE);
+
+        DBMS_OUTPUT.PUT_LINE('LA DUREE TOTAL DU VOL EST : ''' 
+            || MINUTES
+            ||''' MINUTES, '''
+            || HEURES
+            ||''' HEURES, ET '''
+            || JOURS
+            ||''' JOURS. '''
+        
+            );
+
+
+        
+
 
 END;
 /
